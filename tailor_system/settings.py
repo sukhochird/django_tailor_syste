850fc9d4f4e2227f3a2f8e4ce7fc540d0f70cf4c
dsukhochir <dsukhochir@gmail.com>
2025-10-25 18:12:43 +0800

    task refactor

--- tailor_system/settings.py
+++ tailor_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4(ue93#gyx1(c7a3wnn9kh-%0o*b3qk-i2ig&()vvm+3#1j91g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'tailor_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ninjeestailor',
+        'USER': 'gegee',
+        'PASSWORD': 'dellF912;',
+        'HOST': '46.101.198.19',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
